f182624accaa4a6dad9b23373cfa1ede5f6687fd
TheNewRoot <126024362+TheNewR00t@users.noreply.github.com>
2024-09-21 16:01:38 +0200

    Update Scp012Firts.cs

--- AddMoreCodeToPlayer/Scp012Firts.cs
+++ AddMoreCodeToPlayer/Scp012Firts.cs
@@ -1,11 +1,7 @@
 ﻿using Exiled.API.Features;
 using Mirror;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace AddMoreCodeToPlayer
